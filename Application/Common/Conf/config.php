f165c8d1915c6a8153afe380779e486dc30876b0
terry <2654126245@qq.com>
2016-08-26 09:26:17 +0800

    提交初始代码

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,22 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => 'carshop',
+  'DB_USER' => 'root',
+  'DB_PWD' => '',
+  'DB_PORT' => 3306,
+  'DB_PREFIX' => 'cs_',
+  'DB_CHARSET' => 'utf8',
+  'DOMAIN' => 'htpp://127.0.0.1/carshop1',
+  'TITLE' => '我的网站2',
+  'KEYWORDS' => 'thinkphp 童老师',
+  'DESCRIPTION' => '童老师thikphp视频教程222',
+  'EMAIL' => 'tong@163.com',
+  'CONTACT' => '童攀111',
+  'FAX' => '12456585',
+  'PHONE' => '1378165***',
+  'ICP' => '备********号',
+  'COPY' => '童攀版权所有',
+  'ADDRESS' => '中国北京123',
+); ?>
